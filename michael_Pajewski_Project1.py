fc7772fd748b64ef117f26cc1437f77eaeddfa46
mtp9k <mtp9k@virginia.edu>
2020-07-28 12:57:28 -0400

    updating files

--- michael_Pajewski_Project1.py
+++ michael_Pajewski_Project1.py
@@ -1,20 +1,21 @@
 #testing file for project 1 ideas
 #Mike Pajewski 
 
- 
-import pandas as pd
 import numpy as np
+import pandas as pd
+import plotly.offline as pyo
+import plotly.express as px
 
-
+#setting notbook to run offline 
+pyo.init_notebook_mode()
 
 #import data file
 df = pd.read_csv('cleanest_data.csv') 
 
+#grouping by area of interest 
+df_intrest = df.groupby(['Area']).sum()
 
-ax1 = df.plot.scatter(x='DateDonated',
-                      y='NumberofAttributes')
-
-ax1.set_ylim(0,100)
-
-
-
+# plot histigram of count of area of interest
+fig = px.histogram(df, x="Area",
+                   title="Histogram of Area")
+fig.show()
